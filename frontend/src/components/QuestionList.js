0539842c321bca84f32ef513e0de7cee6b830468
globaltempest <ericyifwang@gmail.com>
2023-05-15 18:13:13 -0400

    cached questionlist and memorized components

--- frontend/src/components/QuestionList.js
+++ frontend/src/components/QuestionList.js
@@ -1,48 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState, memo, useMemo } from 'react';
 import axios from 'axios';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-class QuestionList extends React.Component {
-    state = {questions: [],}
+function QuestionList() {
+  const [questions, setQuestions] = useState([]);
 
-    componentDidMount() {
-        axios.get('http://127.0.0.1:8000/polls/questions/')
+  const memoizedQuestions = useMemo(() => {
+    return questions.map(question => (
+      <QuestionRow key={question.id} question={question} />
+    ))
+  }, [questions]);
+
+  useEffect(() => {
+    const cachedQuestions = localStorage.getItem('cachedQuestions');
+    if (cachedQuestions) {
+      setQuestions(JSON.parse(cachedQuestions));
+    } else {
+      const startTime = Date.now();
+      axios.get('http://127.0.0.1:8000/polls/questions/')
         .then(res => {
-          console.log(res); // log the response object to the console
+          const endTime = Date.now();
+          const responseTime = endTime - startTime;
+          console.log(`Response time: ${responseTime}ms`);
           const questions = res.data;
-          this.setState({ questions });
+          localStorage.setItem('cachedQuestions', JSON.stringify(questions));
+          setQuestions(questions);
         })
-        .catch(err => {console.log(err)})
-      }
-    
-      render() {
-        return (
-          <div className='container-fluid mt-4 row justify-content-center'>
-            <header className="text-center display-1 font-weight-bold title ">Questions</header>
-            <table class="table table-hover table-responsive caption-top text-center w-50 mt-5">
-              <caption className='text-center'><h1>Polls</h1></caption>
-              <tbody className='table-group-divider '>
-                {this.state.questions.map(question =>
-                  <tr key={question.id}>
-                    <td>
-                      <Link to={`/${question.id}/vote/`}>{question.question_text}</Link>
-                    </td>
-                  </tr>
-                )}
-              </tbody>
-            </table>
-          </div>
-        );
-      }
-}
-export default QuestionList;
+        .catch(err => { console.log(err) })
+    }
+  }, []);
 
-/*
-<ul>
-    {this.state.questions.map(question =>
-      <li key={question.id}>
+  return (
+    <div className='container-fluid mt-4 row justify-content-center'>
+      <header className="text-center display-1 font-weight-bold title ">Questions</header>
+      <table className="table table-hover table-responsive caption-top text-center w-50 mt-5">
+        <caption className='text-center'><h1>Polls</h1></caption>
+        <tbody className='table-group-divider '>
+          {memoizedQuestions}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+const QuestionRow = memo(({ question }) => {
+  return (
+    <tr>
+      <td>
         <Link to={`/${question.id}/vote/`}>{question.question_text}</Link>
-      </li>
-    )}
-              </ul>
-*/
+      </td>
+    </tr>
+  );
+});
+
+export default QuestionList;
--- frontend/src/components/VoteQuestion.js
+++ frontend/src/components/VoteQuestion.js
@@ -1,68 +1,80 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo, useCallback } from 'react';
 import axios from 'axios';
 import {useParams} from 'react-router-dom';
 import QuestionResults from './QuestionResults';
 
 function VoteQuestion() {
-    const {id} = useParams();
+    const { id } = useParams();
     const [question, setQuestion] = useState(null);
     const [selectedChoice, setSelectedChoice] = useState(null);
     const [hasVoted, setHasVoted] = useState(false);
-
+  
     useEffect(() => {
-        axios.get(`http://127.0.0.1:8000/polls/questions/${id}/`)
+      const startTime = Date.now();
+      axios.get(`http://127.0.0.1:8000/polls/questions/${id}/`)
         .then(res => {
-            setQuestion(res.data);
+          const endTime = Date.now();
+          const responseTime = endTime - startTime;
+          console.log(`Response time: ${responseTime}ms`);
+          setQuestion(res.data);
         })
         .catch(err => { })
     }, [id]);
-
-    const handleSubmit = e => {
-        e.preventDefault();
-        if (!selectedChoice) {
-            return;
-        }
-
-        axios.post(`http://127.0.0.1:8000/polls/questions/${id}/vote/`, {choice: selectedChoice})
+  
+    const handleSubmit = useCallback((e) => {
+      e.preventDefault();
+      if (!selectedChoice) {
+        return;
+      }
+  
+      const startTime = Date.now();
+      axios.post(`http://127.0.0.1:8000/polls/questions/${id}/vote/`, { choice: selectedChoice })
         .then(res => {
-            setHasVoted(true);
-            setQuestion(res.data);
+          const endTime = Date.now();
+          const responseTime = endTime - startTime;
+          console.log(`Response time: ${responseTime}ms`);
+          setHasVoted(true);
+          setQuestion(res.data);
         })
-        .catch(err => {console.log(err)})
-    };
-
-    const handleChoiceChange = e => {
+        .catch(err => { console.log(err) })
+    }, [id, selectedChoice]);
+  
+    const handleChoiceChange = useCallback(e => {
         setSelectedChoice(e.target.value);
-    };
-
-    if (!question) {
+    }, []);
+  
+    const memoizedComponent = useMemo(() => {
+      if (!question) {
         return <div>Loading ...</div>
-    }
-
-    if (hasVoted) {
-        return <QuestionResults question={question}/>
-    }
-
-    return (
+      }
+  
+      if (hasVoted) {
+        return <QuestionResults question={question} />
+      }
+  
+      return (
         <div>
-            <header className="text-center display-1 font-weight-bold title mt-5">{question.question_text}</header>
-            <div className="d-flex align-items-center justify-content-center mt-5">
-                <form onSubmit={handleSubmit}>
-                    <ul className='list-unstyled'>
-                        {question.choices.map(choice => (
-                            <li key = {choice.id}>
-                                <label>
-                                    <input type='radio' name='choice' value={choice.id} onChange={handleChoiceChange}/>
-                                    <span style={{ marginLeft: '10px', font: '1.2rem' }}>{choice.choice_text}</span>
-                                </label>
-                            </li>
-                        ))}
-                    </ul>
-                    <button type='submit' className='btn btn-dark btn-lg'>Vote</button>
-                </form>
-            </div>
+          <header className="text-center display-1 font-weight-bold title mt-5">{question.question_text}</header>
+          <div className="d-flex align-items-center justify-content-center mt-5">
+            <form onSubmit={handleSubmit}>
+              <ul className='list-unstyled'>
+                {question.choices.map(choice => (
+                  <li key={choice.id}>
+                    <label>
+                      <input type='radio' name='choice' value={choice.id} onChange={handleChoiceChange} />
+                      <span style={{ marginLeft: '10px', font: '1.2rem' }}>{choice.choice_text}</span>
+                    </label>
+                  </li>
+                ))}
+              </ul>
+              <button type='submit' className='btn btn-dark btn-lg'>Vote</button>
+            </form>
+          </div>
         </div>
-    );
-}
-
-export default VoteQuestion;
+      );
+    }, [question, hasVoted, handleSubmit, handleChoiceChange]);
+  
+    return memoizedComponent;
+  }
+  
+  export default VoteQuestion;
